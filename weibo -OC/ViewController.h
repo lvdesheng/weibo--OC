5c68affe8f6fe55b7e7efc1d76e41edaf5177de3
lvdesheng <lvdesheng123@icloud.com>
2016-03-01 11:40:10 +0800

    Initial Commit

---
+++ weibo -OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo -OC
+//
+//  Created by lvdesheng on 16/3/1.
+//  Copyright © 2016年 lvdesheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weibo -OC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  weibo -OC
+//
+//  Created by lvdesheng on 16/3/1.
+//  Copyright © 2016年 lvdesheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
