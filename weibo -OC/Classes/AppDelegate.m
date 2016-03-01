9e2f4f961ef90700eaee04aa53c78221c2387762
lvdesheng <lvdesheng123@icloud.com>
2016-03-01 16:23:05 +0800

    添加导航控制器

--- weibo -OC/Classes/AppDelegate.m
+++ weibo -OC/Classes/AppDelegate.m
@@ -7,10 +7,8 @@
 //
 
 #import "AppDelegate.h"
-#import "LVHomeViewController.h"
-#import "LVDiscoverViewController.h"
-#import "LVMeViewController.h"
-#import "LVMessageViewController.h"
+#import "LVTabBarController.h"
+
 
 @interface AppDelegate ()
 
@@ -26,58 +24,20 @@
     
     //设置根控制器
     
-    UITabBarController *tabBarVC = [[UITabBarController alloc]init];
+    LVTabBarController *tabBarVC = [[LVTabBarController alloc]init];
 
     self.window.rootViewController = tabBarVC;
 
     
     [self.window makeKeyAndVisible];
     
-    //设置子控制器
-    
-    //首页
-    LVHomeViewController *homeVC = [[LVHomeViewController alloc]init];
-    [self addChildController:homeVC title:@"首页" image:@"tabbar_home" selImage:@"tabbar_home_selected"];
-    //消息
-    LVMessageViewController *MessageVC = [[LVMessageViewController alloc]init];
-    [self addChildController:MessageVC title:@"消息" image:@"tabbar_message_center" selImage:@"tabbar_message_center"];
-    //发现
-    LVDiscoverViewController *discoverVC = [[LVDiscoverViewController alloc]init];
-    [self addChildController:discoverVC title:@"发现" image:@"tabbar_discover" selImage:@"tabbar_discover_selected"];
-    //我
-    LVMeViewController *meVC = [[LVMeViewController alloc]init];
-    [self addChildController:meVC title:@"我" image:@"tabbar_profile" selImage:@"tabbar_profile_selected"];
-    
-    [tabBarVC addChildViewController:homeVC];
-    [tabBarVC addChildViewController:MessageVC];
-    [tabBarVC addChildViewController:discoverVC];
-    [tabBarVC addChildViewController:meVC];
+
     
     return YES;
 }
 
 
-- (void)addChildController:(UIViewController *)childVC title:(NSString *)title image:(NSString *)image selImage:(NSString *)selImage
-{
-    //设置tabBar的图片
 
-    childVC.tabBarItem.title = title;
-    childVC.tabBarItem.image = [UIImage imageNamed:image];
-    childVC.tabBarItem.selectedImage = [UIImage imageWithRenderingName:selImage];
-    //字体颜色
-    NSMutableDictionary *TextAttr = [NSMutableDictionary dictionary];
-    TextAttr[NSForegroundColorAttributeName] = LVColor(123, 123, 123);
-    
-    [childVC.tabBarItem setTitleTextAttributes:TextAttr forState:UIControlStateNormal];
-    
-    NSMutableDictionary *selTextAttr = [NSMutableDictionary dictionary];
-    selTextAttr[NSForegroundColorAttributeName] = [UIColor orangeColor];
-    [childVC.tabBarItem setTitleTextAttributes:selTextAttr forState:UIControlStateSelected];
-    
-    childVC.view.backgroundColor = LVRandomColor;
-    
-
-}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
---
+++ weibo -OC/Classes/LVTabBarController.m
@@ -0,0 +1,71 @@
+//
+//  LVTabBarController.m
+//  weibo -OC
+//
+//  Created by lvdesheng on 16/3/1.
+//  Copyright © 2016年 lvdesheng. All rights reserved.
+//
+
+#import "LVTabBarController.h"
+#import "LVHomeViewController.h"
+#import "LVDiscoverViewController.h"
+#import "LVMeViewController.h"
+#import "LVMessageViewController.h"
+
+
+@interface LVTabBarController ()
+
+@end
+
+@implementation LVTabBarController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    //首页
+    LVHomeViewController *homeVC = [[LVHomeViewController alloc]init];
+    [self addChildController:homeVC title:@"首页" image:@"tabbar_home" selImage:@"tabbar_home_selected"];
+    //消息
+    LVMessageViewController *MessageVC = [[LVMessageViewController alloc]init];
+    [self addChildController:MessageVC title:@"消息" image:@"tabbar_message_center" selImage:@"tabbar_message_center"];
+    //发现
+    LVDiscoverViewController *discoverVC = [[LVDiscoverViewController alloc]init];
+    [self addChildController:discoverVC title:@"发现" image:@"tabbar_discover" selImage:@"tabbar_discover_selected"];
+    //我
+    LVMeViewController *meVC = [[LVMeViewController alloc]init];
+    [self addChildController:meVC title:@"我" image:@"tabbar_profile" selImage:@"tabbar_profile_selected"];
+    
+
+}
+
+
+- (void)addChildController:(UIViewController *)childVC title:(NSString *)title image:(NSString *)image selImage:(NSString *)selImage
+{
+    //设置tabBar的图片
+    childVC.title = title;// 同时设置tabbar和navigationBar的文字
+//    childVC.tabBarItem.title = title;
+    
+    
+    childVC.tabBarItem.image = [UIImage imageNamed:image];
+    childVC.tabBarItem.selectedImage = [UIImage imageWithRenderingName:selImage];
+    //字体颜色
+    NSMutableDictionary *TextAttr = [NSMutableDictionary dictionary];
+    TextAttr[NSForegroundColorAttributeName] = LVColor(123, 123, 123);
+    
+    [childVC.tabBarItem setTitleTextAttributes:TextAttr forState:UIControlStateNormal];
+    
+    NSMutableDictionary *selTextAttr = [NSMutableDictionary dictionary];
+    selTextAttr[NSForegroundColorAttributeName] = [UIColor orangeColor];
+    [childVC.tabBarItem setTitleTextAttributes:selTextAttr forState:UIControlStateSelected];
+    
+    childVC.view.backgroundColor = LVRandomColor;
+    
+    
+    //设置子控制器
+    UINavigationController *navVC = [[UINavigationController alloc]initWithRootViewController:childVC];
+    
+    [self addChildViewController:navVC];
+}
+
+@end
