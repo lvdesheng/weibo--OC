269db927e943a98e25cdf73e3a61ba36be55c147
lvdesheng <lvdesheng123@126.com>
2016-03-02 14:14:40 +0800

    弹出下拉菜单

--- weibo -OC/Classes/Home(首页)/Controller/LVHomeViewController.m
+++ weibo -OC/Classes/Home(首页)/Controller/LVHomeViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "LVHomeViewController.h"
-#import "UIButton+ImageTitleSpacing.h"
+
 
 
 
@@ -21,23 +21,7 @@
     [super viewDidLoad];
 
     //设置导航栏的上面内容
-//    self.navigationItem.leftBarButtonItem =[UIBarButtonItem itemWithImage:[UIImage imageNamed:@"navigationbar_friendsearch"] highImage:[UIImage imageNamed:@"navigationbar_friendsearch_highlighted"] addTarget:self action:@selector(friendAdd)];
-    
-//    UIButton *btn = [[UIButton alloc]init];
-//    
-//    [btn setImage:[UIImage imageNamed:@"navigationbar_friendsearch"] forState:UIControlStateNormal];
-//    [btn setTitle:@"加好友" forState:UIControlStateNormal];
-//    [btn sizeToFit];
-//    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-//    btn.contentEdgeInsets = UIEdgeInsetsMake(0, -20, 0, 0);
-//    btn.titleLabel.font = [UIFont systemFontOfSize:10];
-//    UIView *contentView = [[UIView alloc] initWithFrame:btn.bounds];
-//    CGFloat space = 1;
-//    [btn layoutButtonWithEdgeInsetsStyle:MKButtonEdgeInsetsStyleTop imageTitleSpace:space];
-//    [btn addTarget:self action:@selector(friendAdd) forControlEvents:UIControlEventTouchUpInside];
-//    [contentView addSubview:btn];
-//    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:contentView];
-    
+   
     self.navigationItem.leftBarButtonItem = [UIBarButtonItem leftItemWithImage:[UIImage imageNamed:@"navigationbar_friendsearch"] highImage:[UIImage imageNamed:@"navigationbar_friendsearch_highlighted"] addTarget:self action:@selector(friendAdd) title:@"加好友" space:0 fontOfSize:10 MKButtonEdgeInsetsStyle:MKButtonEdgeInsetsStyleTop];
     
     
@@ -45,6 +29,28 @@
     
     self.navigationItem.rightBarButtonItem = [UIBarButtonItem rightItemWithImage:[UIImage imageNamed:@"navigationbar_pop"] highImage:[UIImage imageNamed:@"navigationbar_pop_highlighted"] addTarget:self action:@selector(pop) title:@"扫一扫" space:0 fontOfSize:10 MKButtonEdgeInsetsStyle:MKButtonEdgeInsetsStyleTop];
 
+    
+    /*中间标题按钮*/
+    UIButton *button = [[UIButton alloc]init];
+    button.width = 150;
+    button.height = 30;
+//    button.backgroundColor = LVRandomColor;
+    
+    //设置图片和文字
+    [button setTitle:@"首页" forState:UIControlStateNormal];
+    [button setImage:[UIImage imageNamed:@"navigationbar_arrow_down"] forState:UIControlStateNormal];
+    [button setImage:[UIImage imageNamed:@"navigationbar_arrow_up"] forState:UIControlStateSelected];
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    button.titleLabel.font = [UIFont systemFontOfSize:17];
+    
+    //button的图片和title调换位置
+    CGFloat space = 2;
+    [button layoutButtonWithEdgeInsetsStyle:MKButtonEdgeInsetsStyleRight imageTitleSpace:space];
+    
+    [button addTarget:self action:@selector(buttonClick) forControlEvents:UIControlEventTouchUpInside];
+    
+    self.navigationItem.titleView = button;
+    
 
     
 }
@@ -59,6 +65,33 @@
 {
     LVFunc
 }
+
+- (void)buttonClick
+{
+     // 这样获得的窗口，是目前显示在屏幕最上面的窗口
+    // self.view.window = [UIApplication sharedApplication].keyWindow
+    // 建议使用[UIApplication sharedApplication].keyWindow获得窗口
+    UIWindow *window = [[UIApplication sharedApplication].windows lastObject];
+    
+    //添加蒙版(用来拦截灰色以外的点击事件)
+    UIView *cover = [[UIView alloc]init];
+    cover.backgroundColor = [UIColor clearColor];
+    cover.frame = window.frame;
+    [window addSubview:cover];
+    
+    //添加带箭头的灰色图片
+    UIImageView *dropdownMenu = [[UIImageView alloc]init];
+    dropdownMenu.image = [UIImage imageNamed:@"popover_background"];
+    dropdownMenu.width = 217;
+    dropdownMenu.height = 217;
+    dropdownMenu.y = 40;
+    
+    [dropdownMenu addSubview:[UIButton buttonWithType:UIButtonTypeInfoLight]];
+    [window addSubview:dropdownMenu];
+    
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
