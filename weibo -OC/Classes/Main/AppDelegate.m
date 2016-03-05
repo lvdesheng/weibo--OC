711873d30df8ab6194c4e4664dea11f9feef0757
lvdesheng <lvdesheng123@126.com>
2016-03-05 12:37:50 +0800

    获取AccessToken

--- weibo -OC/Classes/Main/AppDelegate.m
+++ weibo -OC/Classes/Main/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import "LVTabBarController.h"
 #import "LVNewFeatureController.h"
+#import "LVOAuthController.h"
 
 @interface AppDelegate ()
 
@@ -24,27 +25,27 @@
     
     //设置根控制器
     
-    LVNewFeatureController *tabBarVC = [[LVNewFeatureController alloc]init];
+    LVOAuthController *OAuthController = [[LVOAuthController alloc]init];
 
-    self.window.rootViewController = tabBarVC;
+    self.window.rootViewController = OAuthController;
     
 //    //设置启动逻辑
-    NSString *key = @"CFBundleVersion";
-    //上一次使用的版本号(存在沙盒)
-    NSString *lastVersion =  [[NSUserDefaults standardUserDefaults] objectForKey:key];
-    //当前版本号(从info.plist中获取)
-    NSString *currentVersion = [NSBundle mainBundle].infoDictionary[key];
-    
-    if ([currentVersion isEqualToString:lastVersion]){//版本号相同,这次打开的和上次打开的相同
-        self.window.rootViewController = [[LVTabBarController alloc]init];
-    }else{//和上次打开的不一样
-        self.window.rootViewController = [[LVNewFeatureController alloc]init];
-    
-        //将当前版本号存入沙盒
-        [[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:key];
-        //刷新
-        [[NSUserDefaults standardUserDefaults]synchronize];
-    }
+//    NSString *key = @"CFBundleVersion";
+//    //上一次使用的版本号(存在沙盒)
+//    NSString *lastVersion =  [[NSUserDefaults standardUserDefaults] objectForKey:key];
+//    //当前版本号(从info.plist中获取)
+//    NSString *currentVersion = [NSBundle mainBundle].infoDictionary[key];
+//    
+//    if ([currentVersion isEqualToString:lastVersion]){//版本号相同,这次打开的和上次打开的相同
+//        self.window.rootViewController = [[LVTabBarController alloc]init];
+//    }else{//和上次打开的不一样
+//        self.window.rootViewController = [[LVNewFeatureController alloc]init];
+//    
+//        //将当前版本号存入沙盒
+//        [[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:key];
+//        //刷新
+//        [[NSUserDefaults standardUserDefaults]synchronize];
+//    }
     
 
     
--- weibo -OC/Classes/OAuth授权/LVOAuthController.m
+++ weibo -OC/Classes/OAuth授权/LVOAuthController.m
@@ -7,8 +7,9 @@
 //
 
 #import "LVOAuthController.h"
+#import "AFNetworking.h"
 
-@interface LVOAuthController ()
+@interface LVOAuthController ()<UIWebViewDelegate>
 
 @end
 
@@ -16,22 +17,112 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    //1.创建一个webView
+    UIWebView *WebView = [[UIWebView alloc]init];
+    WebView.frame = self.view.bounds;
+    WebView.delegate = self;
+    
+    [self.view addSubview:WebView];
+    
+    
+    //用webView加载登陆界面
+    //https://api.weibo.com/oauth2/authorize
+    /**
+     client_id	true	string	申请应用时分配的AppKey。
+     redirect_uri	true	string	授权回调地址，站外应用需与设置的回调地址一致，站内应用需填写canvas page的地址。
+     */
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.weibo.com/oauth2/authorize?client_id=1266961611&redirect_uri=http://www.baidu.com"];
+    
+    NSURLRequest *Request = [NSURLRequest requestWithURL:url];
+
+    [WebView loadRequest:Request];
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+#pragma mark - UIWebViewDelegate
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+        //获取url
+    NSString *url = request.URL.absoluteString;
+    
+    
+    //判断是否是回调地址
+    NSRange range = [url rangeOfString:@"code="];
+    
+    if (range.length !=0){//是回调地址
+        //截取code=后面的参数值
+        NSUInteger fromIndex = range.length + range.location;
+        NSString *code = [url substringFromIndex:fromIndex];
+        
+        //利用code换取一个accessToken
+        [self accessTokenWithCode:code];
+        NSLog(@"%@,%@",url,code);
+    }
+    
+    NSLog(@"%@",url);
+    return YES;
+    
 }
 
-/*
-#pragma mark - Navigation
+/**
+ *  利用code（授权成功后的request token）换取一个accessToken
+ *
+ *  @param code 授权成功后的request token
+ */
+- (void)accessTokenWithCode:(NSString *)code
+{
+    /**
+     https://api.weibo.com/oauth2/access_token
+     client_id	true	string	申请应用时分配的AppKey。
+     client_secret	true	string	申请应用时分配的AppSecret。
+     grant_type	true	string	请求的类型，填写authorization_code
+     
+     grant_type为authorization_code时
+     必选	类型及范围	说明
+     code	true	string	调用authorize获得的code值。
+     redirect_uri	true	string	回调地址，需需与注册应用里的回调地址一致。
+     text/plain
+     */
+    
+    //请求管理者
+    AFHTTPSessionManager *mgr = [AFHTTPSessionManager manager];
+    mgr.responseSerializer = [AFJSONResponseSerializer serializer];
+    //请求参数
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    parameters[@"client_id"] = @"1266961611";
+    parameters[@"client_secret"] = @"331390a9396a0cc7f879b5a5a9eb5ded";
+    parameters[@"grant_type"] = @"authorization_code";
+    parameters[@"code"] = code;
+    parameters[@"redirect_uri"] = @"http://www.baidu.com";
+    
+ 
+    //发送请求
+    [mgr POST:@"https://api.weibo.com/oauth2/access_token" parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+        NSLog(@"请求成功%@",responseObject);
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"请求失败%@",error);
+    }];
+    
+    
 }
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
