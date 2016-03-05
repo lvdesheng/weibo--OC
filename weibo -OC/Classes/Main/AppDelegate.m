e52489464ceac47f6e3564c94c160ee1df4f9836
lvdesheng <lvdesheng123@126.com>
2016-03-05 09:11:34 +0800

    afn

--- weibo -OC/Classes/Main/AppDelegate.m
+++ weibo -OC/Classes/Main/AppDelegate.m
@@ -29,22 +29,22 @@
     self.window.rootViewController = tabBarVC;
     
 //    //设置启动逻辑
-//    NSString *key = @"CFBundleVersion";
-//    //上一次使用的版本号(存在沙盒)
-//    NSString *lastVersion =  [[NSUserDefaults standardUserDefaults] objectForKey:key];
-//    //当前版本号(从info.plist中获取)
-//    NSString *currentVersion = [NSBundle mainBundle].infoDictionary[key];
-//    
-//    if ([currentVersion isEqualToString:lastVersion]){//版本号相同,这次打开的和上次打开的相同
-//        self.window.rootViewController = [[LVTabBarController alloc]init];
-//    }else{//和上次打开的不一样
-//        self.window.rootViewController = [[LVNewFeatureController alloc]init];
-//    
-//        //将当前版本号存入沙盒
-//        [[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:key];
-//        //刷新
-//        [[NSUserDefaults standardUserDefaults]synchronize];
-//    }
+    NSString *key = @"CFBundleVersion";
+    //上一次使用的版本号(存在沙盒)
+    NSString *lastVersion =  [[NSUserDefaults standardUserDefaults] objectForKey:key];
+    //当前版本号(从info.plist中获取)
+    NSString *currentVersion = [NSBundle mainBundle].infoDictionary[key];
+    
+    if ([currentVersion isEqualToString:lastVersion]){//版本号相同,这次打开的和上次打开的相同
+        self.window.rootViewController = [[LVTabBarController alloc]init];
+    }else{//和上次打开的不一样
+        self.window.rootViewController = [[LVNewFeatureController alloc]init];
+    
+        //将当前版本号存入沙盒
+        [[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:key];
+        //刷新
+        [[NSUserDefaults standardUserDefaults]synchronize];
+    }
     
 
     
